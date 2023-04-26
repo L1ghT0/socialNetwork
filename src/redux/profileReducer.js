d01f754d6b8eee4a5a040d257657db1650d1b97c
Pavel <zvjxrf@yandex.ru>
2023-04-26 13:09:50 -0400

    new unit tests were added for profileReducer

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,6 +3,7 @@ import {profileApi, usersApi} from "../api/api";
 const ADD_POST = 'ADD_POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
 const SET_STATUS = 'SET_STATUS'
+const DELETE_POST = 'DELETE_POST'
 
 
 let initialState = {
@@ -31,6 +32,11 @@ const profileReducer = (state = initialState, action) => {
                 ...state,
                 postsData: [...state.postsData, newPost],
             }
+        case DELETE_POST:
+            return {
+                ...state,
+                postsData: state.postsData.filter(post => post.id !== action.postId)
+            }
         case SET_USER_PROFILE:
             return {
                 ...state,
@@ -48,6 +54,8 @@ const profileReducer = (state = initialState, action) => {
 }
 
 export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText});
+export const deletePost = (postId) => ({type: DELETE_POST, postId});
+
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
 
--- src/redux/profileReducer.test.js
+++ src/redux/profileReducer.test.js
@@ -1,4 +1,4 @@
-import profileReducer, {addPostActionCreator} from "./profileReducer";
+import profileReducer, {addPostActionCreator, deletePost} from "./profileReducer";
 
 
 let initialState = {
@@ -14,4 +14,31 @@ it('length of posts should be increased', function () {
     let newState = profileReducer(initialState, action);
 
     expect(newState.postsData.length).toBe(3);
-}); 
+});
+
+it('message of new post should be correct', function () {
+    let action = addPostActionCreator("New post text")
+
+    let newState = profileReducer(initialState, action);
+
+    expect(newState.postsData[2].postText).toBe("New post text");
+});
+
+it('after deleting the length of posts should be decreased', function () {
+    let action = deletePost(1)
+
+    let newState = profileReducer(initialState, action);
+
+    expect(newState.postsData.length).toBe(1);
+});
+
+it(`after deleting the length of posts should not be decreased if id is incorrect`, function () {
+    // 1. test data
+    let action = deletePost(1000) // '1000' is incorrect id
+
+    // 2. action
+    let newState = profileReducer(initialState, action);
+
+    // 3. expectation
+    expect(newState.postsData.length).toBe(2);
+});
