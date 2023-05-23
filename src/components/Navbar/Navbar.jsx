67f3c315b59f325f2742ff36c6be999f6525c376
Pavel <zvjxrf@yandex.ru>
2023-05-23 16:48:03 -0400

    icons in navbar have been added and stylized

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,22 +6,40 @@ const Navbar = () => {
     return (
         <nav className={Nb_classes.nav}>
             <div>
-                <NavLink to='/profile/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>Profile</NavLink>
+                <NavLink to='/profile/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>
+                    <div className={Nb_classes.spriteUser + ' ' + Nb_classes.sprites}></div>
+                    Profile
+                </NavLink>
             </div>
             <div>
-                <NavLink to='/dialogs/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>Message</NavLink>
+                <NavLink to='/dialogs/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>
+                    <div className={Nb_classes.spriteMessage + ' ' + Nb_classes.sprites}></div>
+                    Message
+                </NavLink>
             </div>
             <div className={Nb_classes.item}>
-                <a>News</a>
+                <NavLink to='/news/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>
+                    <div className={Nb_classes.spriteNews + ' ' + Nb_classes.sprites}></div>
+                    News
+                </NavLink>
             </div>
             <div className={Nb_classes.item}>
-                <a>Music</a>
+                <NavLink to='/music/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>
+                    <div className={Nb_classes.spriteMusicalNote + ' ' + Nb_classes.sprites}></div>
+                    Music
+                </NavLink>
             </div>
             <div>
-                <NavLink to='/users/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>Find User</NavLink>
+                <NavLink to='/users/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>
+                    <div className={Nb_classes.spriteGroup + ' ' + Nb_classes.sprites}></div>
+                    Find User
+                </NavLink>
             </div>
             <div className={Nb_classes.item}>
-                <a>Settings</a>
+                <NavLink to='/settings/' className={navData => navData.isActive ? Nb_classes.active : Nb_classes.item}>
+                    <div className={Nb_classes.spriteSetting + ' ' + Nb_classes.sprites}></div>
+                    Settings
+                </NavLink>
             </div>
         </nav>
     );
