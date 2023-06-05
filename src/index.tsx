8d3af6715664fbaa0e16285207cc36f12f448e20
Pavel <zvjxrf@yandex.ru>
2023-06-05 16:52:57 -0400

    BrowserRouter has been replaced by HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import store from "./redux/reduxStore";
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
 
